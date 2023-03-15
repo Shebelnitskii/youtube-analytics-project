11d945c26f572ded42309322c7600f052d32c819
Shebel <Shebelnitskiy@gmail.com>
2023-03-15 19:32:04 +0400

    add function class Channel and adding tests

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,6 @@
 from googleapiclient.discovery import build
 import os
+import json
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -10,7 +11,37 @@ class Channel:
         api_key: str = os.getenv('YT_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=api_key)
 
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(channel)
+
+    @property
+    def title(self):
+        """Возвращает название канала."""
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel['items'][0]['snippet']['title']
+
+    @property
+    def video_count(self):
+        """Возвращает количество видео в канале."""
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel['items'][0]['statistics']['videoCount']
+
+    @property
+    def url(self):
+        """Возвращает url канала."""
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
+
+    def get_service():
+        """Возвращает сервис для работы с каналами."""
+        youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+        return youtube
+
+    def to_json(self,file):
+        """ Сохраняет данные канала в файл json """
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        with open(file, 'w') as f:
+            json.dump(channel, f)
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,5 +1,7 @@
 import pytest
 from src.channel import Channel
+from googleapiclient.discovery import build
+import os
 
 @pytest.fixture()
 def channel():
@@ -10,3 +12,15 @@ def test_channel_init(channel):
 
 def test_channel_print(channel):
     assert channel.print_info() == None
+
+def test_title(channel):
+    assert channel.title == 'вДудь'
+
+def test_video_count(channel):
+    assert channel.video_count == '165'
+
+def test_url(channel):
+    assert channel.url == 'https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA'
+
+def test_to_json(channel):
+    assert channel.to_json('test') == None
